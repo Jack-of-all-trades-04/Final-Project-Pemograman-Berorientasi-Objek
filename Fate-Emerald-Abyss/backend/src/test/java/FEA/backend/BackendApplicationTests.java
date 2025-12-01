a7704a1fe2a6e19eaa11629a3049a52146eeaa60
Jack-of-all-trades-04 <novanagung08@gmail.com>
2025-12-01 07:56:23 +0700

    Initialize Project - LibGDX and Springboot

---
+++ Fate-Emerald-Abyss/backend/src/test/java/FEA/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package FEA.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
